f537e9a27ea0ef43b8990263ec1bcdbf99b86f13
ann-ngambi <ngambiann08@gmail.com>
2025-02-18 23:42:07 +0300

    First component

---
+++ src/components/CarItems.vue
@@ -0,0 +1,18 @@
+<script>
+export default{
+    data(){
+        return{
+            message:'You won!',
+            name:'Lewis'
+        };
+    }
+};
+</script>
+<template>
+<div>
+    <h1>{{ message }}</h1>
+    <p>{{ name }}</p>
+</div>
+
+</template>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import CarItems from './components/CarItems.vue'
+
+const app = createApp(App)
+app.component('Car-Items',CarItems)
+app.mount('#app')
